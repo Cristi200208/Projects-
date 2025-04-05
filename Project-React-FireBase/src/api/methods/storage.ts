bd80992afe80dac12085ed6ffb845fac61c0bbb3
Cristi200208 <150088974+Cristi200208@users.noreply.github.com>
2025-04-05 22:31:10 +0300

    gg

--- Project-React-FireBase/src/api/methods/storage.ts
+++
@@ -1,9 +0,0 @@
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
-import { storage } from "../firebase";
-
-export const uploadImage = async (file: File) => {
-  const storageRef = ref(storage, `products/${file.name}`);
-  await uploadBytes(storageRef, file);
-  const url = await getDownloadURL(storageRef);
-  return url;
-};
--- Project-React-FireBase/src/providers/AuthContext.tsx
+++
@@ -1,4 +0,0 @@
-import { createContext } from "react";
-import { AuthContextProps } from "../types/users";
-
-export const AuthContext = createContext<AuthContextProps | null>(null);
